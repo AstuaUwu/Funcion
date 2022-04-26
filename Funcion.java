2b16d9776e1e5cc004521f4e93c914b4c4bb6666
AstuaUwu <102199181+AstuaUwu@users.noreply.github.com>
2022-04-25 21:14:37 -0600

    Add files via upload

---
+++ Funcion.java
@@ -0,0 +1,16 @@
+public class Funcion {
+public static void main(String args[]){
+    int D;
+    
+    D=Multiplicar(5,5);
+    
+    System.out.println("El resultado es " + D);
+}
+static int Multiplicar(int a, int b){
+        
+        return a*b;
+        
+    
+    }
+}    
+
